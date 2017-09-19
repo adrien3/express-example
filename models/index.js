697d89934251a8a433bfae7266b2ea23169b1533
Joao Veiga <joao.rodriguesveiga@altran.com>
2017-09-19 15:57:11 +0200

    Suppression code mort

--- models/index.js
+++ models/index.js
@@ -1,17 +1,13 @@
 "use strict";
 
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var env       = "development";
-var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+const fs        = require("fs");
+const path      = require("path");
+const Sequelize = require("sequelize");
+const env       = "development";
+const config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
 
-if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL,config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-var db        = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const db        = {};
 
 fs
   .readdirSync(__dirname)
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
-var models  = require('../models');
-var express = require('express');
-var router  = express.Router();
+const models  = require('../models');
+const express = require('express');
+const router  = express.Router();
 
 router.get('/', function(req, res) {
   models.User.findAll({
     include: [ models.Task ]
   }).then(function(users) {
-    console.log("INDEX");
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ users: users });
   });
 });
 
