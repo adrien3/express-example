9e4b5f2ce1d4ae19bec39e0219cc7d1ff91780e2
Joao Veiga <joao.rodriguesveiga@altran.com>
2017-10-02 15:10:01 +0200

    add route User

--- routes/tasks.js
+++ routes/tasks.js
@@ -32,5 +32,63 @@ router.get('/:id_task', (req, res) => {
 });
 
 
+/**
+ * Create a new task, return the new task object
+ * @param task : Task object to create
+ * @return New task object created
+ */
+router.post('', (req, res) => {
+  if(req.body){
+    models.Task.create(req.body).then(new_task => {
+      res.json(new_task);
+    })
+    .catch((error) => {
+      // error 
+      res.json(error);
+    });
+  }
+});
+
+
+/**
+ * Task object edition by given id, return modified task object
+ * @param id_task: Id to find task object to modify
+ * @param task : Task object to create
+ * @return Return task object modified
+ */
+router.put('/:id_task', (req, res) => {
+  if(req.body){
+    models.Task.update(req.body, { 
+      where: { id: req.params.id_task }
+     }).then(edit_task => {
+        models.Task.findById(req.params.id_task).then((edit_task) => {
+          res.json(edit_task);
+        });
+    })
+    .catch((error) => {
+      // error 
+      res.json(error);
+    });
+  }
+});
+
+
+
+/**
+ * Close object task by given id
+ * @param id_task: Id to find the task object to close
+ * @return Task object closed
+ */
+router.delete('/:id_task', (req, res) => {
+    models.Task.destroy({
+      where: { id:  req.params.id_task }
+    }).then(delete_task => {
+      res.json(delete_task);
+    })
+    .catch((error) => {
+      // error 
+      res.json(error);
+    });
+});
 
 module.exports = router;
